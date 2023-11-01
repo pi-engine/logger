4f4d0a1130abcace2f99c2cc795cf1e403081f7d
kerloper <karimijavad990@gmail.com>
2023-11-01 04:50:40 +0330

    add interface of inventory log reader

--- src/Repository/LogRepositoryInterface.php
+++ src/Repository/LogRepositoryInterface.php
@@ -2,7 +2,10 @@
 
 namespace Logger\Repository;
 
+use Laminas\Db\ResultSet\HydratingResultSet;
+
 interface LogRepositoryInterface
 {
     public function addUser(array $params = []): void;
+    public function readInventoryLog(array $params = []): HydratingResultSet|array ;
 }
