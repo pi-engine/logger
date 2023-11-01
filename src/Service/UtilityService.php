694446c17de5e9b2189d91d611dda09cc8bdaaf3
kerloper <karimijavad990@gmail.com>
2023-11-01 05:09:15 +0330

    improve canonize

--- src/Service/UtilityService.php
+++ src/Service/UtilityService.php
@@ -9,7 +9,7 @@ use Logger\Service\ServiceInterface;
 
 class UtilityService implements ServiceInterface
 {
-    public function __construct( )
+    public function __construct()
     {
     }
 
@@ -24,7 +24,32 @@ class UtilityService implements ServiceInterface
 
     public function inventoryLogCanonize($object): array
     {
-        return ['id' => -1];
+        if (empty($object)) {
+            return [];
+        }
+
+        if (is_object($object)) {
+            $object = [
+                'id' => (int)$object->getId(),
+                'timestamp' => $object->getTimestamp(),
+                'priority' => $object->getPriority(),
+                'priority_name' => $object->getPriorityName(),
+                'message' => $object->getMessage(),
+                'extra_data' => $object->getExtraData(),
+            ];
+        } else {
+            $object = [
+                'id' => (int)$object['id'],
+                'timestamp' => $object['timestamp'],
+                'priority' => $object['priority'],
+                'priority_name' => $object['priorityName'],
+                'message' => $object['message'],
+                'extra_data' => $object['extra_data']
+            ];
+        }
+        $object['extra_data'] = json_decode($object['extra_data'], true);
+        return $object;
+
     }
 
 }
