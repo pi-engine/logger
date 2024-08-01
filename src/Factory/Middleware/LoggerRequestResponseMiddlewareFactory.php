00a00e624284bcb2d3165233e5f6b177fc1bf7aa
Hossein Azizabadi Farahani <hossein@azizabadi.com>
2024-08-01 13:53:53 +0330

    Add new logger middleware for save both of request and response params as log

---
+++ src/Factory/Middleware/LoggerRequestResponseMiddlewareFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Logger\Factory\Middleware;
+
+use Interop\Container\Containerinterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Logger\Middleware\LoggerRequestResponseMiddleware;
+use Logger\Service\LoggerService;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use User\Handler\ErrorHandler;
+
+class LoggerRequestResponseMiddlewareFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null): LoggerRequestResponseMiddleware
+    {
+        return new LoggerRequestResponseMiddleware(
+            $container->get(ResponseFactoryInterface::class),
+            $container->get(StreamFactoryInterface::class),
+            $container->get(ErrorHandler::class),
+            $container->get(LoggerService::class)
+        );
+    }
+}
---
+++ src/Middleware/LoggerRequestResponseMiddleware.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Logger\Middleware;
+
+use Logger\Service\LoggerService;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use User\Handler\ErrorHandler;
+
+class LoggerRequestResponseMiddleware implements MiddlewareInterface
+{
+    /** @var ResponseFactoryInterface */
+    protected ResponseFactoryInterface $responseFactory;
+
+    /** @var StreamFactoryInterface */
+    protected StreamFactoryInterface $streamFactory;
+
+    /** @var ErrorHandler */
+    protected ErrorHandler $errorHandler;
+
+    /** @var LoggerService */
+    protected LoggerService $loggerService;
+
+    public function __construct(
+        ResponseFactoryInterface $responseFactory,
+        StreamFactoryInterface $streamFactory,
+        ErrorHandler $errorHandler,
+        LoggerService $loggerService
+    ) {
+        $this->responseFactory = $responseFactory;
+        $this->streamFactory   = $streamFactory;
+        $this->errorHandler    = $errorHandler;
+        $this->loggerService   = $loggerService;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        // Call the next middleware or handler
+        $response = $handler->handle($request);
+
+        // Post-handler logic
+        $this->writeRequestResponse( $request,  $response);
+
+        return $response;
+    }
+
+    private function writeRequestResponse(ServerRequestInterface $request, ResponseInterface $response): void
+    {
+        // Get attributes
+        $attributes = $request->getAttributes();
+
+        // Get route information
+        $routeMatch  = $request->getAttribute('Laminas\Router\RouteMatch');
+        $routeParams = $routeMatch->getParams();
+
+        // Set message
+        $message = sprintf(
+            '%s-%s-%s-%s',
+            $routeParams['module'],
+            $routeParams['section'],
+            $routeParams['package'],
+            $routeParams['handler']
+        );
+
+        // Set log params
+        $params = [
+            'user_id'    => $attributes['account']['id'] ?? 0,
+            'company_id' => $attributes['company_authorization']['company_id'] ?? 0,
+            'route'      => $routeParams,
+            'request'    => [
+                'method'          => $request->getMethod(),
+                'uri'             => (string)$request->getUri(),
+                'headers'         => $request->getHeaders(),
+                'body'            => (string)$request->getBody(),
+                'protocolVersion' => $request->getProtocolVersion(),
+                'serverParams'    => $request->getServerParams(),
+                'queryParams'     => $request->getQueryParams(),
+                'parsedBody'      => $request->getParsedBody(),
+                'uploadedFiles'   => $request->getUploadedFiles(),
+                'cookies'         => $request->getCookieParams(),
+                'attributes'      => $request->getAttributes(),
+                'target'          => $request->getRequestTarget(),
+            ],
+            'response'   => [
+                'body'            => $response->getBody(),
+                'headers'         => $response->getHeaders(),
+                'protocolVersion' => $response->getProtocolVersion(),
+                'encodingOptions' => $response->getEncodingOptions(),
+                'payload'         => $response->getPayload(),
+                'reasonPhrase'    => $response->getReasonPhrase(),
+                'statusCode'      => $response->getStatusCode(),
+            ],
+        ];
+
+        // Set log
+        $this->loggerService->write($message, $params);
+    }
+}
